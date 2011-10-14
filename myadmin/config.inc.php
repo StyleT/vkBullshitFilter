e45cc5e1daa85c36a41cb2aaa4c83275dc388fee
StyleT <Vlad Fedosov@vlad.fedosov@gmail.com>
2011-10-15 00:05:13 +0300

    fix5 /myadmin/config.inc.php

--- myadmin/config.inc.php
+++ myadmin/config.inc.php
@@ -35,8 +35,6 @@ $cfg['Servers'][$i]['compress'] = false;
 /* Select mysqli if your server has it */
 $cfg['Servers'][$i]['extension'] = 'mysql';
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
-$cfg['Servers'][$i]['user'] = 'jacqueline';
-$cfg['Servers'][$i]['password'] = 'E2U35tk0';
 
 /*
  * phpMyAdmin configuration storage settings.
