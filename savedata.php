06e609c68807c1a82e5cf9140dd92122171bc32a
StyleT <Vlad Fedosov@vlad.fedosov@gmail.com>
2011-10-14 22:20:37 +0300

    test DB

--- savedata.php
+++ savedata.php
@@ -1,4 +1,18 @@
 <?php
 
 echo "Hello world!";
+
+$mysqli = new mysqli( '/tmp/mysql/santos.sock',  'jacqueline',
+                      'E2U35tk0', 'santos' );
+if (!$mysqli) {
+    die('Could not connect mysqli: ' . mysql_error());
+}
+echo 'Connected successfully mysqli';
+
+$link = mysql_connect('localhost:/tmp/mysql/santos.sock', 'jacqueline', 'E2U35tk0');
+if (!$link) {
+    die('Could not connect mysql: ' . mysql_error());
+}
+echo 'Connected successfully mysql';
+mysql_close($link);
 ?>
