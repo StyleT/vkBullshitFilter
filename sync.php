9de09126b9ad124c07b6fcb7e7715ab8598aa040
StyleT <Vlad Fedosov@vlad.fedosov@gmail.com>
2011-10-15 22:21:08 +0300

    Add sync functional 0.5 (fixed)

--- sync.php
+++ sync.php
@@ -3,7 +3,7 @@
 $mysql_user="root";
 $mysql_pass="";
 $mysql_db="vkbullshit.my";*/
-echo "here4";
+
 $mysql_host=$_SERVER['MYSQL_HOST'];
 $mysql_user=$_SERVER['MYSQL_USER'];
 $mysql_pass=$_SERVER['MYSQL_PASS'];
@@ -11,14 +11,12 @@ $mysql_db=$_SERVER['MYSQL_DB'];
 $data=json_decode($_POST["data"]);
 $task=$_POST["task"];
 ///
-echo "here3";
+
 //Подключаемся к БД
 $db = mysql_connect($mysql_host, $mysql_user, $mysql_pass);
 if (!$db) {die('Could not connect mysql: ' . mysql_error());}
 mysql_select_db($mysql_db, $db) or die ('Can\'t use foo : ' . mysql_error());
-echo "here2";
 if($task=="fullsync"){
-	echo "here1";
 	echo fullSync($data);
 }
 mysql_close($db);
@@ -38,7 +36,6 @@ function fullSync($data){
 			$query.=", ";
 		$query.="('".$data[$i]->link."')";
 	}
-	echo $query;//////
 	mysql_query($query)or die("Invalid query [".$query."]: " . mysql_error());
 	$query="SELECT link FROM `blacklist` WHERE link NOT IN (";
 	for($i=0; $i<count($data); $i++){
@@ -47,7 +44,6 @@ function fullSync($data){
 		$query.="'".$data[$i]->link."'";
 	}
 	$query.=")";
-	echo $query;////
 	$new_entries= mysql_query($query)or die("Invalid query [".$query."]: " . mysql_error());
 	$new_entries= mysql_fetch_all($new_entries);
 	return json_encode($new_entries);
